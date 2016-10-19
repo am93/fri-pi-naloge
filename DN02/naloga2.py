a1036f41586612c10b039559995aed44e47c1e3b
Anze Medved <am93@users.noreply.github.com>
2016-10-19 16:08:54 +0200

    Naloga2 v0.2: narejen prvi del, skoraj dokončan drugi del

--- DN02/naloga2.py
+++ DN02/naloga2.py
@@ -1,6 +1,7 @@
 import unidecode
 from collections import Counter
 from my_clustering import HierarchicalClustering
+import math
 
 
 __author__ = "Anze Medved, 63120191"
@@ -42,6 +43,30 @@ class LanguageSimilarity:
             if len(idx) == 1: ids[idx[0][0]] = idx[0][1]
         self.lang_ids = ids
 
+    def identify_language(self, filename):
+        """
+        Function identifies language from given filename
+        """
+        text = self.read_file(filename)
+        pairs = Counter(self.tuples(text))
+        results = {k:self.cosine_distance(pairs,v) for k,v in self.languages.items()}
+        sorted_results = sorted(results.items(), key=lambda x: x[1])
+        return sorted_results
+
+    def print_identification_results(self, original, r):
+        print('-------------------------------')
+        print('Original text was in: ', original)
+        preds = "".join('('+self.lang_ids[r[i][0]]+', '+str(r[i][1])+') ' for i in range(3))
+        print('Predicted languages and distances: ', preds)
+
+    @staticmethod
+    def cosine_distance(lang1, lang2):
+        inter_key = set(lang1.keys()).intersection(set(lang2.keys()))
+        dot_prod = float(sum([lang1[key] * lang2[key] for key in inter_key]))
+        len_vec1 = float(math.sqrt(sum([math.pow(lang1[key], 2) for key in lang1.keys()])))
+        len_vec2 = float(math.sqrt(sum([math.pow(lang2[key], 2) for key in lang2.keys()])))
+        return 1 - (dot_prod / (len_vec1 * len_vec2))
+
     @staticmethod
     def tuples(string, k=2):
         """
@@ -70,12 +95,22 @@ class LanguageSimilarity:
 
 
 if __name__ == "__main__":
-    ls = LanguageSimilarity(['Slovenian', 'Russian', 'Albanian', 'Macedonian', 'Serbian (Latin)',
-                             'Hungarian', 'Finnish', 'Polish', 'Greek', 'Turkish'], 'translations/')
+    # Prvi del naloge
+    ls = LanguageSimilarity(['Slovenian', 'Russian', 'Lithuanian', 'Estonian', 'Macedonian', 'Serbian (Latin)',
+                             'Polish', 'Greek', 'Bulgarian', 'Icelandic', 'Filipino', 'Italian', 'Latvian',
+                             'Luxembourgish', 'German', 'Norwegian (Nynorsk)', 'Swedish', 'Romanian',
+                             'Belorus', 'Slovak', 'Bosnian (Cyrillic script)', 'Japanese', 'Corsican',
+                             'Chinese', 'Javanese', 'Spanish', 'Portuguese'], 'translations/')
     hc = HierarchicalClustering(ls.languages, ls.lang_ids, HierarchicalClustering.cosine_distance,
                                 HierarchicalClustering.average_linkage)
     hc.compute_clusters()  # create clusters
     hc.create_dendrogram()  # create dendrogram based on clusters
     hc.dendro.create_leaves()  # add leaf nodes to dendrogram (single countries)
     hc.prepare_visualization()
-    [print(len(ls.languages[id])) for id in ls.lang_ids.keys()]
+
+    # Drugi del naloge
+    real = ['Japanese', 'Swedish', 'Slovenian', 'Spanish', 'German', 'Icelandic']
+    for i in range(0,6):
+        lang = ls.identify_language('samples/'+str(i)+'.txt')
+        ls.print_identification_results(real[i], lang)
+
